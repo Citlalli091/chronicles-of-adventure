eda23931c5d590c09a8c7c552108e71d414aec9b
Brayan Rocha <brayan84rocha@gmail.com>
2023-06-06 13:43:10 -0700

    reworked signup funcitonality

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,89 +1,89 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 
-// export const userInfo = () => {
-//   const [signupForm, setSignupForm] = useState({
-//     username: '',
-//     email: '',
-//     password: ''
-//   });
+// // export const userInfo = () => {
+// //   const [signupForm, setSignupForm] = useState({
+// //     username: '',
+// //     email: '',
+// //     password: ''
+// //   });
 
 
-  const handleSignupChange = (e) => {
-    setSignupForm({
-      ...signupForm,
-      [e.target.name]: e.target.value
-    });
-  };
+//   const handleSignupChange = (e) => {
+//     setSignupForm({
+//       ...signupForm,
+//       [e.target.name]: e.target.value
+//     });
+//   };
 
 
-  const handleSignupSubmit = (e) => {
-    e.preventDefault();
-    // Add signup form submission logic here
-    console.log('Signup form submitted:', signupForm);
+//   const handleSignupSubmit = (e) => {
+//     e.preventDefault();
+//     // Add signup form submission logic here
+//     console.log('Signup form submitted:', signupForm);
  
-    const errors = {
-      uname: "invalid username",
-      pass: "invalid password"
-    };
+//     const errors = {
+//       uname: "invalid username",
+//       pass: "invalid password"
+//     };
     
-    const handleSubmit = (event) => {
-      //Prevent page reload
-      event.preventDefault();
+//     const handleSubmit = (event) => {
+//       //Prevent page reload
+//       event.preventDefault();
     
-      var { uname, pass } = document.forms[0];
+//       var { uname, pass } = document.forms[0];
     
-      // Find user login info
-      const userData = database.find((user) => user.username === uname.value);
+//       // Find user login info
+//       const userData = database.find((user) => user.username === uname.value);
     
-      // Compare user info
-      if (userData) {
-        if (userData.password !== pass.value) {
-          // Invalid password
-          setErrorMessages({ name: "pass", message: errors.pass });
-        } else {
-          setIsSubmitted(true);
-        }
-      } else {
-        // Username not found
-        setErrorMessages({ name: "uname", message: errors.uname });
-      }
-    };
+//       // Compare user info
+//       if (userData) {
+//         if (userData.password !== pass.value) {
+//           // Invalid password
+//           setErrorMessages({ name: "pass", message: errors.pass });
+//         } else {
+//           setIsSubmitted(true);
+//         }
+//       } else {
+//         // Username not found
+//         setErrorMessages({ name: "uname", message: errors.uname });
+//       }
+//     };
 
-  return (
-    <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSignupSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={signupForm.username}
-          onChange={handleSignupChange}
-        />
-        <br />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={signupForm.email}
-          onChange={handleSignupChange}
-        />
-        <br />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={signupForm.password}
-          onChange={handleSignupChange}
-        />
-        <br />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>Sign Up</h2>
+//       <form onSubmit={handleSignupSubmit}>
+//         <input
+//           type="text"
+//           name="username"
+//           placeholder="Username"
+//           value={signupForm.username}
+//           onChange={handleSignupChange}
+//         />
+//         <br />
+//         <input
+//           type="email"
+//           name="email"
+//           placeholder="Email"
+//           value={signupForm.email}
+//           onChange={handleSignupChange}
+//         />
+//         <br />
+//         <input
+//           type="password"
+//           name="password"
+//           placeholder="Password"
+//           value={signupForm.password}
+//           onChange={handleSignupChange}
+//         />
+//         <br />
+//         <button type="submit">Sign Up</button>
+//       </form>
+//     </div>
+//   );
+// };
 
 
-export default Signup;
+// export default Signup;
 
